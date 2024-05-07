09caf936384d96161d310d86bc939cd6c3641a82
0x009922 <43530070+0x009922@users.noreply.github.com>
2024-05-07 14:42:03 +0900

    feat: use SVG logos, add links, support dark theme (#72)
    * feat: use SVG logos, add links
    
    Signed-off-by: Dmitry Balashov <43530070+0x009922@users.noreply.github.com>
    
    * feat: adapt to dark theme
    
    Signed-off-by: Dmitry Balashov <43530070+0x009922@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dmitry Balashov <43530070+0x009922@users.noreply.github.com>

--- .vitepress/theme/components/CompaniesLogos.vue
+++ .vitepress/theme/components/CompaniesLogos.vue
@@ -7,41 +7,109 @@ defineProps<{
 <template>
   <div
     class="logos"
-    :class="{
-      'flex items-center justify-center ': location === 'docs',
-      'space-y-4': location === 'aside',
-    }"
     :data-location="location"
   >
-    <img
-      src="/orillion-logo.png"
-      alt="Orillion logo"
-      width="100"
+    <a
+      href="https://www.orillionsolutions.com/"
+      target="_blank"
+      class="item"
     >
-    <img
-      src="/soramitsu-logo-1.png"
-      alt="Soramitsu logo"
-      width="140"
-    >
+      <div class="orillion-crop">
+        <img
+          src="/orillion-logo.svg"
+          alt="Orillion logo"
+        >
+      </div>
+    </a>
+    <div>
+      <a
+        class="item"
+        href="https://soramitsu.co.jp/"
+        target="_blank"
+      >
+        <div class="soramitsu-crop">
+          <img
+            src="/SORAMITSU-red-main-landscape.svg"
+            alt="Soramitsu logo"
+          >
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .logos {
   margin: 16px 0;
-  padding: 16px;
-  background-color: var(--vp-c-white);
-  border-radius: 8px;
-  min-width: 0;
+  gap: 8px;
+  display: flex;
+  justify-content: center;
+}
+
+.logos[data-location='aside'] {
+  flex-flow: column;
 }
 
 img {
   min-width: 0;
+  object-fit: cover;
+  transition: filter 0.15s ease;
 }
 
-.logos[data-location='docs'] img + img {
-  padding-left: 8px;
-  margin-left: 8px;
-  border-left: 1px solid var(--vp-c-divider);
+.item {
+  width: 160px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background 0.15s ease;
+}
+
+.item {
+  background: #f9f9f9; /* TODO: use --vt-c-white-soft after VitePress update */
+}
+
+.dark .item {
+  background: #242424; /* TODO: use --vt-c-bg-soft after VitePress update */
+}
+
+.dark .item:hover {
+  background: #f1f1f1;
+}
+
+.dark img {
+  filter: grayscale(1) invert(1);
+}
+
+.item:hover img {
+  filter: none;
+}
+
+/* the following quirks needed to adjust the images,
+   because their source is not normalized */
+
+.soramitsu-crop {
+  width: 140px;
+  height: 40px;
+  overflow: hidden;
+}
+
+.soramitsu-crop img {
+  margin-left: -30px;
+  max-width: none;
+  width: 200px;
+  height: 40px;
+}
+
+.orillion-crop {
+  height: 40px;
+  overflow: hidden;
+}
+
+.orillion-crop img {
+  width: 100px;
+  height: 40px;
+  margin-top: -3px;
+  max-width: none;
 }
 </style>
